4c168051c43a185182c1d843b321d01c51e2ba22
vijaybahadur-bind <vijaybahadur.bind@vernost.com>
2025-04-18 18:58:50 +0530

    Update sonarqube.rb

--- Formula/sonarqube.rb
+++ Formula/sonarqube.rb
@@ -1,9 +1,11 @@
 class Sonarqube < Formula
-  desc "SonarQube - Continuous Code Quality"
+  desc "SonarQube is an open platform to manage code quality"
   homepage "https://www.sonarqube.org/"
   url "https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.4.1.88267.zip"
-  sha256 "c5cc9b93f8a236d8e0e0d4e1466c1f9beaa0d7ae0281d7b6a5a3b870ad1b6c5d"
+  sha256 "8d3b2923d9d947527bb92eaa9d4a9735c0b45066aa0e41c661f30fc3647fa340"
   license "LGPL-3.0"
+
+  depends_on :java => "11+"
 
   def install
     libexec.install Dir["*"]
@@ -15,8 +17,11 @@ class Sonarqube < Formula
       To start SonarQube:
         sonarqube start
 
-      Then open http://localhost:9000 in your browser.
-      Default login: admin / admin
+      To stop SonarQube:
+        sonarqube stop
+
+      Default port: 9000
+      Access via: http://localhost:9000
     EOS
   end
 end
